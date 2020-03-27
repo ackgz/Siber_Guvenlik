4b2b094585b69983065d97ee22177967fa93bc15
ackgz <61361047+ackgz@users.noreply.github.com>
2020-03-27 10:51:17 +0300

    Initial commit

---
+++ SiberGuvenlikOdev/SiberGuvenlikOdev/index.aspx
@@ -0,0 +1,22 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="index.aspx.cs" Inherits="SiberGuvenlikOdev.index" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+    
+        <asp:Button ID="tara" runat="server" OnClick="tara_Click" Text="Tara" />
+    
+        <br />
+        <asp:Label ID="sonuc" runat="server" Height="1000px" Width="1000px"></asp:Label>
+        <br />
+    
+    </div>
+    </form>
+</body>
+</html>
---
+++ SiberGuvenlikOdev/SiberGuvenlikOdev/index.aspx.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace SiberGuvenlikOdev
+{
+    public partial class index : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void tara_Click(object sender, EventArgs e)
+        {
+            ProcessStartInfo process = new ProcessStartInfo();
+            process.Arguments = "-p 1-1024 127.0.0.1";
+            process.RedirectStandardOutput = true;
+            process.FileName = "nmap";
+            Process proces = new Process();
+            proces.StartInfo = process;
+            proces.Start();
+            StreamReader stdout = proces.StandardOutput;
+            StreamWriter sw = new StreamWriter("sonuc.txt");
+            sw.WriteLine(stdout.ReadToEnd());
+            proces.WaitForExit();
+            Console.ReadKey(true);
+
+            StreamReader SR = new StreamReader("sonuc.txt");
+            sonuc.Text = SR.ReadLine();
+            SR.Close();
+        }
+    }
+}
